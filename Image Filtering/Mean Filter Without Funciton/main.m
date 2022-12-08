f36d0b47293114ba9200c1edbdfc264c14be3093
Musab Elibol <116749792+M-Elibol@users.noreply.github.com>
2022-12-08 15:49:16 +0300

    Create main.m

---
+++ Image Filtering/Mean Filter Without Funciton/main.m
@@ -0,0 +1,28 @@
+img=imread ('resim 1.jpg');
+I=rgb2gray(img);
+data = im2double(I);
+output=zeros(length(data),width(data));
+M=input("filtre uzunlugunu giriniz:  ");
+ek=zeros((M-1),width(data));
+newdata=[data;ek];
+x=1;
+sayac2=1;
+z=0;
+deger=length(newdata)-(M-1);
+for p=1 : width(data)
+for  i = 1:deger
+    x=i;
+    for u=1:M
+        y=newdata(x,p);
+        x=x+1;
+        z=z+y;
+    end
+    output(sayac2)= z/3; 
+    sayac2=sayac2+1;
+   z=0;
+end
+end
+figure 
+imshow(data);
+figure 
+imshow(output);
