2e095f572456cf6c9b4b91e9dbafffbdbd65b045
Musab Elibol <116749792+M-Elibol@users.noreply.github.com>
2022-12-08 15:39:12 +0300

    Create main.m

---
+++ Sound Filtering/Mean Filter Without Funciton/main.m
@@ -0,0 +1,23 @@
+[data,fs]= audioread("noisyspeech.wav");
+output=zeros(length(data),1);
+M=input("filtre uzunlugunu giriniz:  ");
+ek=zeros((M-1),1);
+newdata=[data;ek];
+t=(1/fs:1/fs:length(data)/fs);
+x=1;
+sayac2=1;
+z=0;
+deger=length(newdata)-(M);
+for  i = 1:deger
+x=i;
+for u=1:M
+y=newdata(x);
+x=x+1;
+z=z+y;
+end
+output(sayac2)= z/M; 
+sayac2=sayac2+1;
+z=0;
+end
+figure
+plot(t,output)
